9f6fa06b6bd3a8f1582781edf5d4eefddc10a403
esolCursador <Boris.Sotsky@gmail.com>
2015-07-27 12:35:57 +0300

    Added extension project.

---
+++ Src/SolutionCleanup.VSIX/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace GloryS.SolutionCleanup_VSIX
+{
+    static class GuidList
+    {
+        public const string guidSolutionCleanup_VSIXPkgString = "a6558f5c-768c-4221-82a9-c37a81820baa";
+        public const string guidSolutionCleanup_VSIXCmdSetString = "487d5e56-b6a0-43c7-bc16-54593800f3fa";
+        public const string guidToolWindowPersistanceString = "ce835a5b-3a6e-4217-a8c9-cb525b41b1d8";
+
+        public static readonly Guid guidSolutionCleanup_VSIXCmdSet = new Guid(guidSolutionCleanup_VSIXCmdSetString);
+    };
+}
---
+++ Src/SolutionCleanup.VSIX/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace GloryS.SolutionCleanup_VSIX
+{
+    static class PkgCmdIDList
+    {
+        public const uint CleanupSolution =        0x100;
+        public const uint cmdidMyTool =    0x101;
+
+    };
+}
